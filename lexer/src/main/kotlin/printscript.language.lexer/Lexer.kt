4655cc7794a71b731b302199575d18519e75b011
matiaslcoulougian <matias.coulougian@ing.austral.edu.ar>
2023-03-26 12:24:16 -0300

    Lexer refactor wip

--- lexer/src/main/kotlin/printscript.language.lexer/Lexer.kt
+++ lexer/src/main/kotlin/printscript.language.lexer/Lexer.kt
@@ -8,7 +8,11 @@ enum class TokenType {
     NUMBER_TYPE,
     STRING_TYPE,
     PRINTLN,
-    OPERATION,
+    SUM,
+    DIVISION,
+    SUBTRACTION,
+    PRODUCT,
+    EQUALS,
     EOL, // end of line
     EOF // end of file
 }
@@ -17,77 +21,100 @@ class Lexer {
     private var currentPos = 0
     private fun getNextToken(input: String): Token {
         // Skip whitespace characters
-        while (currentPos < input.length && (input[currentPos].isWhitespace() || input[currentPos] == '(' || input[currentPos] == ')')) {
+        while (checkIgnoreChars(input)) {
             currentPos++
         }
 
         // If we have reached the end of file, return EOF token
-        if (currentPos >= input.length) {
-            return Token(TokenType.EOF.name, "")
-        }
+        if (currentPos >= input.length) return Token(TokenType.EOF, "")
+
 
         // If we have reached the end of line, return EOL token
         if (input[currentPos] == ';') {
             currentPos++
-            return Token(TokenType.EOL.name, "")
+            return Token(TokenType.EOL, "")
         }
 
         // Check for identifier or keyword token
         if (input[currentPos].isLetter()) {
-            var value = ""
-            while (currentPos < input.length && (input[currentPos].isLetterOrDigit() || input[currentPos] == '_')) {
-                value += input[currentPos]
-                currentPos++
-            }
-            return when (value) {
-                "let" -> Token(TokenType.DESIGNATOR.name, value)
-                "string" -> Token(TokenType.STRING_TYPE.name, value)
-                "number" -> Token(TokenType.NUMBER_TYPE.name, value)
-                "println" -> Token(TokenType.PRINTLN.name, value)
-                else -> Token(TokenType.IDENTIFIER.name, value)
-            }
+            return checkIdentifier(input)
         }
 
-        // Check for string literal token with "
+        // Check for string literal token with " or '
         if (input[currentPos] == '"' || input[currentPos] == '\'') {
-            val quote = input[currentPos]
-            var value = ""
-            currentPos++
-            while (currentPos < input.length && input[currentPos] != quote) {
-                value += input[currentPos]
-                currentPos++
-            }
-            currentPos++
-            return Token(TokenType.STRING_LITERAL.name, value)
+            return checkStringLiteral(input)
         }
 
         // Check for number literal token
         if (input[currentPos].isDigit()) {
-            var value = ""
-            while (currentPos < input.length && input[currentPos].isDigit()) {
-                value += input[currentPos]
-                currentPos++
-            }
-            return Token(TokenType.NUMBER_LITERAL.name, value)
+            return checkNumberLiteral(input)
         }
 
-        // Check for operation token
-        if (input[currentPos] == '+' || input[currentPos] == '-' || input[currentPos] == '*' || input[currentPos] == '/' || input[currentPos] == '=' || input[currentPos] == ':') {
-            val value = input[currentPos].toString()
-            currentPos++
-            return Token(TokenType.OPERATION.name, value)
+        val token = input[currentPos]
+        currentPos++
+
+        // Check for operation token or equals
+        when(token) {
+            '+' -> return Token(TokenType.SUM)
+            '-' -> return Token(TokenType.SUBTRACTION)
+            '*' -> return Token(TokenType.PRODUCT)
+            '/' -> return Token(TokenType.DIVISION)
+            '=' -> return Token(TokenType.EQUALS)
         }
 
         // If we haven't matched any token, raise an exception
         throw IllegalArgumentException("Invalid character: ${input[currentPos]}")
     }
 
+    private fun checkNumberLiteral(input: String): Token {
+        var value = ""
+        while (currentPos < input.length && input[currentPos].isDigit()) {
+            value += input[currentPos]
+            currentPos++
+        }
+        return Token(TokenType.NUMBER_LITERAL, value)
+    }
+
+    private fun checkStringLiteral(input: String): Token {
+        val quote = input[currentPos]
+        var value = ""
+        currentPos++
+        while (currentPos < input.length && input[currentPos] != quote) {
+            value += input[currentPos]
+            currentPos++
+        }
+        currentPos++
+        return Token(TokenType.STRING_LITERAL, value)
+    }
+
+    private fun checkIdentifier(input: String): Token {
+        var value = ""
+        while (currentPos < input.length && (input[currentPos].isLetterOrDigit() || input[currentPos] == '_')) {
+            value += input[currentPos]
+            currentPos++
+        }
+        return when (value) {
+            "let" -> Token(TokenType.DESIGNATOR)
+            "string" -> Token(TokenType.STRING_TYPE)
+            "number" -> Token(TokenType.NUMBER_TYPE)
+            "println" -> Token(TokenType.PRINTLN)
+            else -> Token(TokenType.IDENTIFIER, value)
+        }
+    }
+
+    private fun checkIgnoreChars(input: String) =
+        currentPos < input.length
+                && (input[currentPos].isWhitespace()
+                || input[currentPos] == '('
+                || input[currentPos] == ')'
+                || input[currentPos] == ':')
+
     fun getAllTokens(input: String): List<Token> {
         val tokens = mutableListOf<Token>()
         while (true) {
             val token = getNextToken(input)
             tokens.add(token)
-            if (token.type == TokenType.EOF.name) {
+            if (token.type == TokenType.EOF) {
                 break
             }
         }
