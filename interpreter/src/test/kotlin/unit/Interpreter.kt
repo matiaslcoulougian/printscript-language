430c6b5e5e9350b61468000c4c1d8e102dee2e11
matiaslcoulougian <matias.coulougian@ing.austral.edu.ar>
2023-04-04 16:20:18 -0300

    Release action and fixes

--- interpreter/src/test/kotlin/unit/Interpreter.kt
+++ interpreter/src/test/kotlin/unit/Interpreter.kt
@@ -1,11 +1,11 @@
-import ast.*
+import ast.* // ktlint-disable no-wildcard-imports
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import printscript.language.interpreter.Interpreter
 import printscript.language.interpreter.InterpreterImpl
 import printscript.language.token.TokenType
 
-class Test {
+class Interpreter {
 
     val interpreter: Interpreter = InterpreterImpl()
 
