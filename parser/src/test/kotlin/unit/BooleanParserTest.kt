be6761670226d0b1528d964c7f3eb83bb35ef49d
mateovalle <mateovalleduran@gmail.com>
2023-04-30 20:30:43 -0300

    solve warning

--- parser/src/test/kotlin/unit/BooleanParserTest.kt
+++ parser/src/test/kotlin/unit/BooleanParserTest.kt
@@ -30,10 +30,6 @@ class BooleanParserTest {
         val trueBoolean = listOf<Token>(Token(TokenType.TRUE))
         val falseBoolean = listOf<Token>(Token(TokenType.FALSE))
         val invalidBoolean = listOf<Token>(Token(TokenType.IDENTIFIER, "true"))
-
-        val trueResult = parser.parseStatement(trueBoolean)
-        val falseResult = parser.parseStatement(falseBoolean)
-
         assertEquals(true, parser.isValidStatement(trueBoolean))
         assertEquals(true, parser.isValidStatement(falseBoolean))
         assertEquals(false, parser.isValidStatement(invalidBoolean))
